39314981392d2476d025817a2bd904bd8b110bd6
Guilherme Athayde <guiathayde303@gmail.com>
2022-05-25 09:54:27 -0300

    Done code

--- main.cpp
+++ main.cpp
@@ -29,4 +29,41 @@ int main(void) {
 void imprime_tabuleiro(int m, int n) {
   int i, j = 0;
   int pb = 2;
+
+  for (i = 0; i < m; i++) { // verificar em qual linha estamos
+    printf("\n");
+
+    if (pb == 0) 
+    {
+      n++;
+      j = 1;
+    }
+    else if (pb == 1)
+    {
+      n--;
+      j = 0;
+    }
+
+    for (; j < n; j++) // verificar em qual coluna estamos
+    {
+      // condicional para verificar as 4 bordas
+      if ((i == 0 && j == 0) || (i == 0 && j == n - 1) || (i == m - 1 && j == 1) || (i == m - 1 && j == n - 1))
+      {
+        printf("[-]");
+        pb = 0;
+      }
+      else if ((j % 2 == 0)) // se par, então preto
+      {
+        printf("[P]");
+        if (j == n - 1)
+          pb = 1;
+      }
+      else // se impar, então branco
+      {
+        printf("[B]");
+        if (j == n - 1)
+          pb = 0;
+      }
+    }
+  }
 }
